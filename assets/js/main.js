ff07436d2e42c4a7774d817fd3cbdd853a365df3
Owen Rumney <owen@owenrumney.co.uk>
2021-07-12 11:51:05 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -671,8 +671,8 @@
     function ajax(url, options) {
         return new Promise(function (resolve, reject) {
             var env = assign({
-                data: null, http://localhost:4000/
-                    method: "GET",
+                data: null,
+                method: "GET",
                 headers: {},
                 xhr: new XMLHttpRequest(),
                 beforeSend: noop,
